1aadb8225daf8ba725a4bd105e6b671811f91d6b
Shahenda Khaled <shahenda.khaled@hotmail.com>
2015-08-25 22:22:46 +0200

    More styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :views
   resources :restaurants
   root'restaurants#index'
-
+  #get 'users/sign_out' => 'devise/sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
