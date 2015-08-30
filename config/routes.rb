8ec585b58238ef02d6ee4191b3c1f7a5a5c20b19
Shahenda Khaled <shahenda.khaled@hotmail.com>
2015-08-30 20:13:41 +0200

    I HATE HEROKU! aka: no more homepage I stayed up for a whole day to figure out! Thank You Very Much

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
     end
     resources :reviews, except: [:show, :index]
   end 
-  # root'restaurants#index'
-  root'welcome#homepage'
+  root'restaurants#index'
+
   #get 'users/sign_out' => 'devise/sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
