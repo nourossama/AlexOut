07aaa6b12e9518ce525e54d3d58e743b3901e518
Shahenda Khaled <shahenda.khaled@hotmail.com>
2015-08-30 19:26:31 +0200

    slideshow edits

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-restaurant = Restaurant.create([{name: 'Nutellopia'}, {image: '<%= image_tag ("nutellopia.jpg" , :alt => "header")%>}''])
+restaurant = Restaurant.create([name: 'Nutellopia'])
